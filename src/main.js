329312e9e65e7931d6ee5a4a48737ef1cd8657fc
sam <sblan45@gmail.com>
2020-10-27 21:55:17 -0400

    adds production mode base url

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import axios from "axios"
 
-axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "https://sharsheret.herokuapp.com/" : "/";
+axios.defaults.baseURL = process.env.NODE_ENV === "production" ? "https://sharsheret.herokuapp.com/" : "/";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
